46e664747b8d2518842295d833078a2a9631cde7
volglizolic <volglizolic@gmail.com>
2020-01-13 19:40:16 +0200

    Platform generic version using c standard and gnu atomic operations.

--- platform.h
+++ platform.h
@@ -11,18 +11,73 @@
 #ifndef PLATFORM_H
 #define PLATFORM_H 1
 
+#include <stdint.h>
+#include "common.h"
 
-#if defined(SPARC) || defined(__sparc__)
-#  include "platform_sparc.h"
-#else /* !SPARC (i.e., x86) */
-#  include "platform_x86.h"
-#endif
+#include <stdatomic.h>
 
 
-#define CAS(m,c,s)  cas((intptr_t)(s),(intptr_t)(c),(intptr_t*)(m))
+/* =============================================================================
+ * Compare-and-swap
+ *
+ * =============================================================================
+ */
+#define CAS(m,c,s)  __sync_val_compare_and_swap(m, c, s)
+
+
+/* =============================================================================
+ * Memory Barriers
+ * =============================================================================
+ */
+#define MEMBARLDLD()   atomic_thread_fence(memory_order_acquire) /* nothing */
+#define MEMBARSTST()   atomic_thread_fence(memory_order_release) /* nothing */
+#define MEMBARSTLD()   atomic_thread_fence(memory_order_acq_rel) /*__asm__ __volatile__ ("membar #StoreLoad" : : :"memory")*/
+
+
+/* =============================================================================
+ * Prefetching
+ *
+ * We use PREFETCHW in LD...CAS and LD...ST circumstances to force the $line
+ * directly into M-state, avoiding RTS->RTO upgrade txns.
+ * =============================================================================
+ */
+__INLINE__ void prefetchw (volatile void* x){
+    __builtin_prefetch((const void *) x);
+}
+
+
+/* =============================================================================
+ * Non-faulting load
+ * =============================================================================
+ */
+__INLINE__ intptr_t
+LDNF (volatile intptr_t* a)
+{
+    return atomic_load_explicit((long*) a, memory_order_seq_cst);
+}
+
+
+/* =============================================================================
+ * MP-polite spinning
+ * -- Ideally we would like to drop the priority of our CMT strand.
+ * =============================================================================
+ */
+#define PAUSE()  /* nothing */
+
+
+/* =============================================================================
+ * Timer functions
+ * =============================================================================
+ */
 
 typedef unsigned long long TL2_TIMER_T;
 
+
+#define TL2_TIMER_READ() ({ \
+struct timespec time; \
+clock_gettime(CLOCK_MONOTONIC, &time); \
+(long)time.tv_sec * 1000000000L + (long)time.tv_nsec; \
+})
 
 #endif /* PLATFORM_H */
 
